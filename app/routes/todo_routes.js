bf4dccb1ec5c61fa0033c0b3c0777a76de7d0886
Menasi Ephrem <menasiephrem@gmail.com>
2017-10-23 15:26:00 +0300

    Finished The routes able to GET, POST, DELETE and patch

--- app/routes/todo_routes.js
+++ app/routes/todo_routes.js
@@ -4,7 +4,24 @@ var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
 
-  app.get('/todos/:id', (req, res) => {
+
+
+// GET /todos  => []  
+
+ app.get('/todos', (req, res) => {
+    const id = req.params.id;
+    db.collection('todos').find().toArray( (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      } 
+    });
+  });
+ 
+// GET /todos/:id  => {}  
+
+app.get('/todos/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
     db.collection('todos').findOne(details, (err, item) => {
@@ -17,8 +34,7 @@ module.exports = function(app, db) {
   });
 
 
- 
-
+// Post /todos  => {}  
 
 app.post('/todos', (req, res) => {
     const note = { task: req.body.task, date: req.body.date , completetd: req.body.completed };
@@ -31,5 +47,41 @@ app.post('/todos', (req, res) => {
     });
   });
 
+
+//Deleted /todos  => {} 
+
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('todos').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('TODO ' + id + ' deleted!');
+      } 
+    });
+  });
+
+
+//Path /todos/:id =>id
+
+app.patch('/todos/:id', function (req, res) {
+    var updateObject = req.body; // {last_name : "smith", age: 44}
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('todos').update(details, {$set: updateObject}, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+       		db.collection('todos').findOne(details, (err, item) => {
+      			if (err) {
+       				 res.send({'error':'An error has occurred'});
+      			} else {
+        			res.send(item);
+     			} 
+    		});
+      	} 
+    });
+});
 
 };
--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
 module.exports = {
   url : "mongodb://todo-db:Pa$$w0rd@ds227045.mlab.com:27045/todo-db"
- 
 };
